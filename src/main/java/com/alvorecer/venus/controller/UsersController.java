3e88f5d8ef36efe7b3062cd2e2576b68d9280b91
WIlliam Silva <wyllyambmg@gmail.com>
2017-03-29 17:00:39 -0300

    Migracao estado e cidade - MG

--- src/main/java/com/alvorecer/venus/controller/UsersController.java
+++ src/main/java/com/alvorecer/venus/controller/UsersController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -25,6 +26,11 @@ public class UsersController {
 
 	@Autowired
 	private RegisterUserService registerUserService;
+	
+	@GetMapping
+	public ModelAndView list(){
+		return new ModelAndView("users/Users");
+	}
 
 	@RequestMapping("/new")
 	public ModelAndView news(Use use) {
